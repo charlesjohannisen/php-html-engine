d13eb4a4b7477faa60d06e306e8a46ce6b4c6a0f
Charles Johannisen <cjohannisen@gmail.com>
2019-01-23 12:12:44 +0200

    cleaning formatting

--- examples/index.php
+++ examples/index.php
@@ -18,18 +18,18 @@ $document->add_scriptfile( "javascript/general.js" );
 $document->add_script( 'alert("hi!");' );
 
 $frag = new FRAGMENT;
-$frag  ->div('{"id":"contents"}')
-    ->div('{"id":"maintext"}')
-      ->table(2)
-        ->tr(3)
-          ->td('{"id":"td1","width":300}',[3])->content("cell1")
-          ->td('{"id":"td1","width":300}',[3])->content("cell2")
-          ->td('{"id":"td1","width":300}',[3])->content("cell3")
-        ->tr(4,[2])
-          ->td('{"id":"td1","width":300}',[4])->content("cell4")
-          ->td('{"id":"td1","width":300}',[4])->content("cell5")
-          ->td('{"id":"td1","width":300}',[4])->content("cell6")
-          ;
+$frag ->div('{"id":"contents"}')
+        ->div('{"id":"maintext"}')
+          ->table(2)
+            ->tr(3)
+              ->td('{"id":"td1","width":300}',[3])->content("cell1")
+              ->td('{"id":"td1","width":300}',[3])->content("cell2")
+              ->td('{"id":"td1","width":300}',[3])->content("cell3")
+            ->tr(4,[2])
+              ->td('{"id":"td1","width":300}',[4])->content("cell4")
+              ->td('{"id":"td1","width":300}',[4])->content("cell5")
+              ->td('{"id":"td1","width":300}',[4])->content("cell6")
+            ;
 
 $document->add_element( $frag->render() );
 
